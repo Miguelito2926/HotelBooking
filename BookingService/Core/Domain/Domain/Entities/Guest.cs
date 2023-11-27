21680ea238db903f07870bc65b2bd32366494c10
Miguelito2926 <juniorprol@hotmail.com>
2023-11-27 17:32:59 -0300

    Add project files.

---
+++ BookingService/Core/Domain/Domain/Entities/Guest.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Domain.Entities
+{
+    public class Guest
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set;}
+        public string Sobrenome { get; set; }
+    }
+}
---
+++ BookingService/Core/Domain/Domain/Entities/Room.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Security.Principal;
+
+namespace Domain.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public bool InMaintenance { get; set; }
+
+        public bool IsAvailable
+        {
+            get
+            {
+                if (InMaintenance || HasGuest)
+                {
+                    return false;
+
+                }
+                return true;
+            }
+        }
+        public bool HasGuest
+        {
+            //Verifica se existem Bookings abertos
+            get { return true; }
+        }
+    }
+}
